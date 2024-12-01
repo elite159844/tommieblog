b05769f4204e8f783070bb5801a0b7665d973d99
Tommie <tommietra@yahoo.com>
2024-12-01 15:59:48 -0500

    push changes

--- src/config.ts
+++ src/config.ts
@@ -31,7 +31,8 @@ export const SITE = {
 	url: "https://tommiesblogs.vercel.app",
 	githubUrl: "https://github.com/elite159844",
 	listDrafts: true,
-	image: "https://github.com/elite159844/tommiesblogs/blob/main/public/images/uploads/tommie.png",
+	image: 
+			"https://github.com/elite159844/tommiesblogs/blob/main/public/tommie.png",
 		     
 	// YT video channel Id (used in media.astro)
 	ytChannelId: "",
@@ -41,7 +42,7 @@ export const SITE = {
 	// Author: Twitter handler
 	authorTwitter: "", // Example: FredKSchott
 	// Author: Image external source
-	authorImage: "https://github.com/elite159844/tommiesblogs/blob/main/public/images/uploads/tommie.png", // Example: https://pbs.twimg.com/profile_images/1272979356529221632/sxvncugt_400x400.jpg, https://abs.twimg.com/sticky/default_profile_images/default_profile_normal.png
+	authorImage: "https://github.com/elite159844/tommiesblogs/blob/main/public/tommie.png",
 	// Author: Bio
 	authorBio:
 		"Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro. Crisp, minimal, personal blog theme for Astro",
